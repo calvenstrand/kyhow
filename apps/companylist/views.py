e6f009898c8bd06453c917eb304ec20069570b1b
clvenstrand <christoffer@rbd.nu>
2012-12-12 15:54:11 +0100

    companylist

--- apps/companylist/views.py
+++ apps/companylist/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.template import RequestContext
+from apps.createpage.models import Company, Education
+
+
+def companyList(request):
+    education_list = Education.objects.all()
+
+    return render_to_response('companylist/companylist.html',
+        {
+            'education_list': education_list
+        },
+        context_instance=RequestContext(request))
