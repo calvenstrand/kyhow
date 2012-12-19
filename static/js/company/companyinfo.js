f5959be99e43db34fa53017bae63b587723d453f
U-mousetrapActual\mouse <mouse@mousetrapActual.(none)>
2012-12-19 13:11:13 +0100

    ll

---
+++ static/js/company/companyinfo.js
@@ -0,0 +1,29 @@
+$(function () {
+
+$('#editCompanyForm').find(':input').each(function () {
+    $(this).attr('disabled', 'disabled');
+});
+
+$('#edit').removeAttr('disabled');
+
+$('#edit').click(function (e) {
+    e.preventDefault();
+    $('#editCompanyForm').find(':input').each(function() {
+        $(this).removeAttr('disabled');
+    })
+    $('#edit').css('display', 'none');
+    $('#save').css('display', 'block');
+    $('#cancel').css('display', 'block');
+})
+
+$('#cancel').click(function (e) {
+    $('#editCompanyForm').find(':input').each(function () {
+        $(this).attr('disabled', 'disabled');
+    })
+    $('#edit').removeAttr('disabled');
+    $('#edit').css('display', 'block');
+    $('#save').css('display', 'none');
+    $('#cancel').css('display', 'none');
+});
+
+});
