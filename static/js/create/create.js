a6feb27702a52d956a40fbda10a7260aa22f1405
clvenstrand <christoffer@rbd.nu>
2013-01-07 12:41:18 +0100

    fixat till lite knappar

---
+++ static/js/create/create.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    $("#createeducation").click(function () {
+        window.location.href = "/createpage/createeducation";
+    });
+
+    $("#createschoolclass").click(function () {
+        window.location.href = "/createpage/createschoolclass";
+    });
+
+    $("#createstudent").click(function () {
+        window.location.href = "/createpage/createstudent";
+    });
+
+    $("#createcourse").click(function () {
+        window.location.href = "/createpage/createcourse";
+    });
+
+    $("#createstep").click(function () {
+        window.location.href = "/createpage/createstep";
+    });
+
+    $("#createcompany").click(function () {
+        window.location.href = "/createpage/createcompany";
+    });
+
+    $("#createcontact_person").click(function () {
+        window.location.href = "/createpage/createcontact_person";
+    });
+
+    $("#createtag").click(function () {
+        window.location.href = "/createpage/createtag";
+    });
+
+});
