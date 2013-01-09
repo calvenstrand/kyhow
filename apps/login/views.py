adb8c8b380f82e8f2e05d511f6d92dd7f5464cd5
Mario Onekanda <mario.onekanda@gmail.com>
2013-01-09 18:42:20 +0100

    removed all files and directory

--- apps/login/views.py
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def index(request):
-    return redirect('/schoolclass/')
