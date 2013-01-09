7f6399685c80f1542129a53ea33081588d390c8a
U-mousetrapActual\mouse <mouse@mousetrapActual.(none)>
2013-01-09 13:48:46 +0100

    took all the notifycode from base and put it inside notifier.js

---
+++ static/js/notifier.js
@@ -0,0 +1,64 @@
+
+var notifys = {
+    init : function( options ) {  },
+    show : function( options ) {  },
+    hide : function( ) {  },
+    notify : function( number ) {
+
+        if (number === 1){//saved forms
+            $("#container").notify("create", "notify-green", {
+                title: 'Formulär sparades!',
+                text: ''
+            });
+        }else if(number === 2){//successchange of step
+            $("#container").notify("create", "notify-green" , {
+                title: 'Inlämningsstatusen förändrades utan fel!',
+                text: ''
+            });
+        }else if(number === 3){
+            $("#container").notify("create" , {
+                title: 'Visar kurs: '+ $('.content').children('b').first().text() +'!',
+                text: ''
+            });
+        }else if(number === 0){
+            $("#container").notify("create" ,"notify-red", {
+                title: 'Formulär kunde inte sparas!',
+                text: ''
+            });
+        }else if(number === 4){//Error changing step
+            $("#container").notify("create" ,"notify-red", {
+                title: 'Inlämningsstatusen kunde inte förändras!',
+                text: ''
+            });
+        }else if(number === 5){
+            $("#container").notify("create" ,"notify-red", {
+                title: 'Du måste fylla i ett namn!',
+            });
+        }
+    },
+};
+
+
+
+    $('#notifydivs').html('<div id="container" style="display:none">'
+        +'<div id="basic-template">'
+        +'<a class="ui-notify-cross ui-notify-close" href="#">x</a>'
+        +'<h1>#{title}</h1><p>#{text}</p></div>'
+        +''
+        +'<div id="notify-green" class="notify-green">'
+        +'<a class="ui-notify-cross ui-notify-close" href="#">x</a>'
+        +'<h1>#{title}</h1></div>'
+        +''
+        +'<div id="course-notify" class="notify-green">'
+        +'<a class="ui-notify-cross ui-notify-close" href="#">x</a>'
+        +'<h1>#{title}</h1></div>'
+        +''
+        +'<div id="notify-red" class="notify-red">'
+        +'<a class="ui-notify-cross ui-notify-close" href="#">x</a>'
+        +'<h1>#{title}</h1>'
+        +'</div>'
+        +''
+        //End of container
+        +'</div>');
+$(function () {
+});
