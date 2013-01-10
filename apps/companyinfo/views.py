d24f3f3fdfb82f793cd8fe81ef91a728aab67ba1
Nicklas Thor <nicke.tor.86@gmail.com>
2013-01-10 16:44:33 +0100

    fixat redigerings knappar för kontaktpersoner på företagsinfo sidan. skapat sidan som hanter redigeringen med länk till backs till rätt företag om regieringen valideras.
    Signed-off-by: Nicklas Thor <nicke.tor.86@gmail.com>

--- apps/companyinfo/views.py
+++ apps/companyinfo/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.shortcuts import render_to_response, get_object_or_404, redirect, render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from apps.createpage.models import Company, Contact_person, Participate
 from apps.companyinfo.forms import CompanyForm
 from django.contrib.auth.decorators import login_required
-
+from apps.createpage.forms import Contact_personForm
 
 #Mainview for the page, holds form and table of contactpersons
 @login_required
@@ -43,3 +43,30 @@ def companyInfo(request, company_id):
             }
             , context_instance=RequestContext(request))
 
+
+@login_required
+def editContactPerson(request, contact_person_id):
+    
+    getContactPerson = get_object_or_404(Contact_person, pk=contact_person_id)
+    
+    if request.method == "POST":
+        form = Contact_personForm(request.POST, instance=getContactPerson or None)
+        if form.is_valid():
+            cmodel = form.save()
+            cmodel.save()
+            
+            return redirect('apps.companyinfo.views.companyInfo', company_id=getContactPerson.company_id.id)
+        
+        else:
+            return render(request, "companyinfo/editContactPerson.html", {
+            'Contact_personForm': form,
+            'Contact_person': getContactPerson,
+            })
+        
+        
+    else:
+        form = Contact_personForm(instance=getContactPerson)
+        return render(request, "companyinfo/editContactPerson.html", {
+        'Contact_personForm': form,
+        'Contact_person': getContactPerson,
+    })
